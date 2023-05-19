509441470956c4e5b0233a11b2e76a94fbbe4410
sovietkilla07 <sovietmoderator109@gmail.com>
2023-05-19 16:58:34 -0300

    tela principal do sistema

--- Sistema_UC12/Program.cs
+++ Sistema_UC12/Program.cs
@@ -6,11 +6,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Endereco end = new Endereco();
-        end.logradouro = "Rua Isabela Luna Tavares";
-        end.numero = 553;
-        end.complemento = "Casa";
-        end.enderecoComercial = false;
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+        Console.WriteLine(@$"
+        #############################################################################
+        |                                                                           |
+        | Seja Bem-Vindo ao sistema de cadastro de pessoa física e pessoa jurídica! |
+        |                                                                           |
+        #############################################################################
+        ");
+        Console.Write("Iniciando");
+        Thread.Sleep(750);
+        Console.Write(".");
+        Thread.Sleep(750);
+        Console.Write(".");
+        Thread.Sleep(750);
+        Console.Write(".");
+        Thread.Sleep(750);
+        Console.Write(".");
+        Thread.Sleep(750);
+        Console.Write(".");
+        Thread.Sleep(750);
+        Console.Write(".");
+        Thread.Sleep(750);
+        Console.Write(".");
+
+
+        Console.ResetColor();
+
+        //Endereco end = new Endereco();
+        //end.logradouro = "Rua Isabela Luna Tavares";
+        //end.numero = 553;
+        //end.complemento = "Casa";
+        //end.enderecoComercial = false;
 
         //PessoaFisica novapf = new PessoaFisica();
 
@@ -43,21 +71,21 @@ class Program
 
         //Console.WriteLine("O endereço do " + novapf.nome + " é " + novapf.endereco.logradouro + ", número " + novapf.endereco.numero + ".");
 
-        PessoaJuridica novaPj = new PessoaJuridica();
-        novaPj.endereco = end;
-        novaPj.nome = "SENAI";
-        novaPj.cnpj = "01234567890001";
-        novaPj.razaoSocial = "Empresa de cosméticos.";
+        //PessoaJuridica novaPj = new PessoaJuridica();
+        //novaPj.endereco = end;
+        //novaPj.nome = "SENAI";
+        //novaPj.cnpj = "01234567890001";
+        //novaPj.razaoSocial = "Empresa de cosméticos.";
 
-        novaPj.validarCNPJ(novaPj.cnpj);
+        //novaPj.validarCNPJ(novaPj.cnpj);
         
-        if (novaPj.validarCNPJ(novaPj.cnpj))
-        {
-            Console.WriteLine("CNPJ válido.");
-        }
-        else
-        {
-            Console.WriteLine("CNPJ inválido.");
-        }
+        //if (novaPj.validarCNPJ(novaPj.cnpj))
+        //{
+            //Console.WriteLine("CNPJ válido.");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("CNPJ inválido.");
+        //}
     }
 }
