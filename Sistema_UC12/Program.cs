f3c4e60a332656a4ff521ef04df628fe5795de85
sovietkilla07 <sovietmoderator109@gmail.com>
2023-05-20 12:29:34 -0300

    case 1 e case 2

--- Sistema_UC12/Program.cs
+++ Sistema_UC12/Program.cs
@@ -58,10 +58,60 @@ class Program
         {
             case "1":
             Console.WriteLine("Você escolheu a opção 1.");
+
+            Endereco end = new Endereco();
+            end.logradouro = "Rua Isabela Luna Tavares";
+            end.numero = 553;
+            end.complemento = "Casa";
+            end.enderecoComercial = false;
+
+            PessoaFisica novapf = new PessoaFisica();
+
+            novapf.cpf = "657.242.782-33";
+            novapf.endereco = end;
+            novapf.dataNascimento = new DateTime(2004, 01, 18);
+            novapf.nome = "Ygor de Andrade Guides";
+
+            Console.WriteLine($"O endereço do {novapf.nome} é {novapf.endereco.logradouro}, número {novapf.endereco.numero}.");
+
+            bool idadeValidada = novapf.validarDataNascimento(novapf.dataNascimento);
+            if(idadeValidada == true){
+                Console.WriteLine($"Pode beber pinga.");
+            }else{
+                Console.WriteLine($"Ainda vai ficar só no refri.");
+            }
+
             break;
 
             case "2":
             Console.WriteLine("Você escolheu a opção 2.");
+
+            PessoaJuridica novaPj = new PessoaJuridica();
+
+            Endereco endPj = new Endereco();
+            endPj.logradouro = "Rua Niteroi";
+            endPj.numero = 180;
+            endPj.complemento = "SENAI";
+            endPj.enderecoComercial = true;
+
+            novaPj.endereco = endPj;
+            novaPj.nome = "SENAI";
+            novaPj.cnpj = "01234567890001";
+            novaPj.razaoSocial = "Empresa de cosméticos.";
+
+            Console.WriteLine($"O endereço do {novaPj.nome} é {novaPj.endereco.logradouro}, número {novaPj.endereco.numero}.");
+
+            novaPj.validarCNPJ(novaPj.cnpj);
+
+            if (novaPj.validarCNPJ(novaPj.cnpj))
+            {
+                Console.WriteLine("CNPJ válido.");
+            }
+            else
+            {
+                Console.WriteLine("CNPJ inválido.");
+            }
+
             break;
 
             case "0":
@@ -79,25 +129,9 @@ class Program
 
         Console.ResetColor();
 
-        //Endereco end = new Endereco();
-        //end.logradouro = "Rua Isabela Luna Tavares";
-        //end.numero = 553;
-        //end.complemento = "Casa";
-        //end.enderecoComercial = false;
+        
 
-        //PessoaFisica novapf = new PessoaFisica();
-
-        //novapf.cpf = "657.242.782-33";
-        //novapf.endereco = end;
-        //novapf.dataNascimento = new DateTime(2004, 01, 18);
-        //novapf.nome = "Ygor de Andrade Guides";
-
-        //bool idadeValidada = novapf.validarDataNascimento(novapf.dataNascimento);
-        //if(idadeValidada == true){
-            //Console.WriteLine($"Pode beber pinga.");
-        //}else{
-            //Console.WriteLine($"Ainda vai ficar só no refri.");
-        //}
+        
 
 
         //Console.WriteLine(novapf.endereco.logradouro);
@@ -116,21 +150,6 @@ class Program
 
         //Console.WriteLine("O endereço do " + novapf.nome + " é " + novapf.endereco.logradouro + ", número " + novapf.endereco.numero + ".");
 
-        //PessoaJuridica novaPj = new PessoaJuridica();
-        //novaPj.endereco = end;
-        //novaPj.nome = "SENAI";
-        //novaPj.cnpj = "01234567890001";
-        //novaPj.razaoSocial = "Empresa de cosméticos.";
-
-        //novaPj.validarCNPJ(novaPj.cnpj);
         
-        //if (novaPj.validarCNPJ(novaPj.cnpj))
-        //{
-            //Console.WriteLine("CNPJ válido.");
-        //}
-        //else
-        //{
-        //    Console.WriteLine("CNPJ inválido.");
-        //}
     }
 }
